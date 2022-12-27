6f90a5a848eee48460a2d8ec09d25b3341a1e9df
陳建佑 <jianyou224@gmail.com>
2022-12-27 11:55:13 +0800

    searchbar component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="wrapper">
     <h1>Github User Search</h1>
-    <div class="wrap">
-      <input type="text" aria-label="1">
-      <button>搜尋</button>
-    </div>
+    <SearchBar></SearchBar>
   </div>
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar.vue';
 
 export default {
   name: 'App',
   components: {
+    SearchBar,
   },
 };
 </script>
@@ -29,25 +28,6 @@ export default {
     color: #fff;
     text-align: center;
     padding: 5rem 0;
-  }
-  .wrap{
-    width: 500px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-    input[type=text] {
-      background: #000;
-      color: #fff;
-      padding: .3rem;
-      outline: none;
-      border: none;
-    }
-    button{
-      background: transparent;
-      border: 1px solid #fa0;
-      color: #fa0;
-      padding: .3rem 1rem;
-    }
   }
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrap">
+    <input type="text" aria-label="1">
+    <button>搜尋</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 500px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+
+  input[type=text] {
+    width: 30%;
+    background: #000;
+    color: #fff;
+    padding: .3rem;
+    outline: none;
+    border: none;
+    transition: .3s;
+
+    &:focus {
+      width: 40%;
+    }
+  }
+
+  button {
+    background: transparent;
+    border: 1px solid #fa0;
+    color: #fa0;
+    padding: .3rem 1rem;
+    transition: .3s;
+
+    &:hover {
+      background: #fa0;
+      color: #111;
+    }
+  }
+}
+</style>
