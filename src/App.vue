332f5d90738458a7fe94227cf4162ea3fc3994a4
陳建佑 <jianyou224@gmail.com>
2022-12-27 11:48:32 +0800

    環境建立

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <h1>Github User Search</h1>
+    <div class="wrap">
+      <input type="text" aria-label="1">
+      <button>搜尋</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0;
+  margin: 0;
+}
+.wrapper{
+  background: #121212;
+  height: 100vh;
+  h1{
+    color: #fff;
+    text-align: center;
+    padding: 5rem 0;
+  }
+  .wrap{
+    width: 500px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    input[type=text] {
+      background: #000;
+      color: #fff;
+      padding: .3rem;
+      outline: none;
+      border: none;
+    }
+    button{
+      background: transparent;
+      border: 1px solid #fa0;
+      color: #fa0;
+      padding: .3rem 1rem;
+    }
+  }
 }
 </style>
